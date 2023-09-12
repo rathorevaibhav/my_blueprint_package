51e2eb299ee395516a62f25eb719b131a29e6461
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-09-12 16:56:46 +0530

    template added

--- my_blueprint/my_blueprint.py
+++ my_blueprint/my_blueprint.py
@@ -1,8 +1,14 @@
 # my_blueprint.py
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-my_blueprint = Blueprint('my_blueprint', __name__)
+my_blueprint = Blueprint('my_blueprint', __name__,
+                         static_folder='static',
+                         template_folder='templates')
 
 @my_blueprint.route('/')
 def index():
     return 'Hello from My Blueprint!'
+
+@my_blueprint.route('/show_template')
+def show_template():
+    return render_template('my_blueprint/my_template.html', message='This is a template message.')
