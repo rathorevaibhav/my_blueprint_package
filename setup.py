fe7e9f4f67a84be413338bc8e5492a95c4d5dacb
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-09-13 17:25:33 +0530

    include package data

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='my_blueprint_package',
     version='0.1',
     packages=find_packages(),
+    include_package_data=True,
     package_data={
         'my_blueprint': ['static/*', 'templates/*'],
     },
